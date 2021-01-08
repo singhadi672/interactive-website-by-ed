fc2956eb7cf5aa2a8385e0034b8d8c399bd3b456
singhadi672 <70839024+singhadi672@users.noreply.github.com>
2021-01-08 11:06:45 +0530

    commit

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 function addSlideScene(){
     let controller = new ScrollMagic.Controller();
     let slides = document.querySelectorAll(".slide");
+    let logo = document.querySelector("#logo");
     let pageScene;
     slides.forEach((slide,index,slides)=>{
         let revealImg = slide.querySelector('#reveal-img');
         let img = slide.querySelector('img');
         let revealText = slide.querySelector('#reveal-text');
-        //gsap
+        // gsap
         const slideT1 = gsap.timeline({
             defaults:{duration:1,ease:'power2.inOut'}
         })
@@ -20,7 +21,7 @@ function addSlideScene(){
             triggerHook:0.25,
             reverse:false
             
-        }).addTo(controller).addIndicators({name:"slide"}).setTween(slideT1);
+        }).addTo(controller).setTween(slideT1);
 
         let nextSlide = slides.length-1===index?'next':slides[index+1];
         const pageT1 = gsap.timeline();
@@ -32,8 +33,98 @@ function addSlideScene(){
             triggerElement: slide,
             duration:'100%',
             triggerHook:0
-        }).addIndicators({indent:200,name:"page",colorStart:"white"}).setTween(pageT1).setPin(slide,{pushFollowers:false}).addTo(controller);
+        }).setTween(pageT1).setPin(slide,{pushFollowers:false}).addTo(controller);
     });
 }
 
-addSlideScene();
+function cursor(e){
+    let cursor = document.querySelector('.cursor');
+    cursor.style.top = e.pageY +'px';
+    cursor.style.left = e.pageX +'px';
+}
+
+function activeCursor(e){
+    let cursor = document.querySelector('.cursor');
+    let cursorText = document.querySelector('.cursor-text');
+    console.log(e.target);
+    if(e.target.id==='logo-burger'||e.target.id==='logo'){
+        cursor.classList.add('cursor-active');
+    }else{
+        cursor.classList.remove('cursor-active');
+    }
+
+    if(e.target.classList.contains('explore')){
+        cursor.classList.add('explore-active');
+        cursorText.innerHTML='Tap';
+
+    }
+    else{
+        cursor.classList.remove('explore-active');
+        cursorText.innerHTML = "";
+    }
+}
+
+window.addEventListener("mousemove",cursor);
+window.addEventListener("mouseover",activeCursor);
+
+let burger = document.querySelector('#logo-burger');
+burger.addEventListener('click',(e)=>{
+    if(!e.target.classList.contains('active')){
+        e.target.classList.add('active');
+        gsap.to('#line1',0.5,{rotate:45,y:5,background:"black"});
+        gsap.to('#logo',0.5,{color:"black"});
+        gsap.to('#line2',0.5,{rotate:-45,y:-5,background:"black"});
+        gsap.to('.nav-bar',1,{clipPath: 'circle(2500px at 100% -10%)'});
+        document.body.classList.add('hide');
+    }else{
+        e.target.classList.remove('active');
+        gsap.to('#line1',0.5,{rotate:0,y:0,background:"white"})
+        gsap.to('#logo',0.5,{color:"white"})
+        gsap.to('#line2',0.5,{rotate:0,y:0,background:"white"})
+        gsap.to('.nav-bar',0.8,{clipPath: 'circle(50px at 100% -10%)'})
+        document.body.classList.remove('hide');
+    }
+})
+
+barba.init({
+    views:[
+        {
+            namespace:'home',
+            beforeEnter(){
+                addSlideScene();
+                logo.href = "./index.html";
+            },
+            beforeLeave(){
+                slideScene.destroy();
+                pageScene.destroy();
+                controller.destroy();
+            }
+        },
+        {
+            namespace:'fashion',
+            beforeEnter(){
+                logo.href = "../index.html";
+            }
+        }
+    ],
+
+    transitions:[
+        {
+            leave({current,next}){
+                let done = this.async();
+                const t1 = gsap.timeline({defaults:{ease:"power2.inOut"}});
+                t1.fromTo(current.container,0.75,{opacity:1},{opacity:0});
+                t1.fromTo('.swipe',0.5,{x:'-100%'},{x:'0%',onComplete:done},)
+            },
+            enter({current,next}){
+                let done = this.async();
+                window.scrollTo(0,0);
+                const t1 = gsap.timeline({defaults:{ease:"power2.inOut"}});
+                t1.fromTo('.swipe',0.5,{x:'0%'},{x:'100%',stagger:0.25,onComplete:done})
+                t1.fromTo(next.container,0.75,{opacity:0},{opacity:1})
+            }
+        }
+
+    ]
+})
+
